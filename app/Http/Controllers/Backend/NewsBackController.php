40bbf299c6db1e498eba1fcf2b545fbccff77a6e
David Chávarri <davidchavarrig@gmail.com>
2021-02-11 19:01:48 -0500

    Fix post 14

--- app/Http/Controllers/Backend/NewsBackController.php
+++ app/Http/Controllers/Backend/NewsBackController.php
@@ -122,7 +122,7 @@ class NewsBackController extends Controller
         $post->status = $request->input('status');
         $post->save();
 
-        return redirect('backend-news/'.$id.'/edit')->with('success', 'Successful update!');
+        return redirect('backend-news')->with('success', 'Successful update!');
 
     }
 
