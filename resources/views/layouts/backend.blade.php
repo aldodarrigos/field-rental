0a38adefcfc8d152434041b0eb1a514a6dd484b2
David Chávarri <davidchavarrig@gmail.com>
2021-02-11 19:28:08 -0500

    Fix post 16

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -32,6 +32,12 @@
 
 <body>
 
+    @php
+        header("Access-Control-Allow-Headers: Authorization, Content-Type");
+        header("Access-Control-Allow-Origin: *");
+        header('content-type: application/json; charset=utf-8');
+    @endphp
+
 <div id="wrapper">
 
     @include('partials.backend.leftnav')
